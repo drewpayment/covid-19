a71d76c9e5bff2cadcfd02024ae9fa407a57f933
Drew Payment <drew@verostack.io>
2020-03-26 22:45:36 -0400

    lets encrypt bs

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const path = require('path');
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
-app.get('/.well-known/acme-challenge/2L1xibuiEBynBga4aKlZfUm66tJVG61vpCsEZHgtfGA', function(req, res) {
-    res.sendFile(path.join(__dirname + 'letsencrypt'));
-});
+app.get('/.well-known/acme-challenge', express.static(path.join(__dirname + 'letsencrypt')));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
