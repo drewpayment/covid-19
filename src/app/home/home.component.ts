92dc8ae10b606103ee651dcd5a7977df6174da40
Drew Payment <drew@verostack.io>
2020-04-11 13:10:16 -0400

    added summary update to home page, updated some UI on countries list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observer, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { NovelCovidService } from '../novelcovid.service';
+import { CoronaSummary } from '../models';
 
 @Component({
     selector: 'app-home',
@@ -11,12 +13,18 @@ import { map } from 'rxjs/operators';
 export class HomeComponent implements OnInit {
 
     isMobile$: Observable<boolean>;
+    covid$: Observable<CoronaSummary>;
 
-    constructor(private bo: BreakpointObserver) { }
+    constructor(
+        private bo: BreakpointObserver,
+        private service: NovelCovidService
+    ) { }
 
     ngOnInit(): void {
         this.isMobile$ = this.bo.observe([ Breakpoints.Handset ])
             .pipe(map(state => state.matches));
+
+        this.covid$ = this.service.getSummary();
     }
 
 }
